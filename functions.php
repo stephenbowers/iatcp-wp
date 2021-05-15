ff9980c73031bcd7453505ec19b8ba6ba79e4886
Stephen Bowers <sb@thatstephenbowers.com>
2021-05-15 14:55:37 -0700

    first commit

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+
+// Add Theme Support
+add_theme_support( 'title-tag' );
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'post_format', ['aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'] );
+add_theme_support( 'html5' );
+add_theme_support( 'automatic-feed-links' );
+add_theme_support( 'custom-background' );
+add_theme_support( 'custom-header' );
+add_theme_support( 'custom-logo' );
+add_theme_support( 'custom-selective-refresh-widgets' );
+add_theme_support( 'starter-content' );
+
+// Load in CSS
+function iatcp_enqueue_styles() {
+
+    wp_enqueue_style( 'main-css', get_stylesheet_directory_uri() . '/style.css', [], time());
+}
+add_action( 'wp_enqueue_scripts', 'iatcp_enqueue_styles' );
+
+?>
