a91456a47dd508c5f351f94e99c26766158c759a
Stephen Bowers <sb@thatstephenbowers.com>
2021-05-15 21:03:59 -0700

    Add footer

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Improv at the Comedy Palace</title>
-</head>
-<body>
-    <h1>index.php</h1>
-</body>
-</html>
+<?php get_header(); ?>
+
+    <h1>Welcome to Improv at the Comedy Palace</h1>
+
+<?php get_footer(); ?>
